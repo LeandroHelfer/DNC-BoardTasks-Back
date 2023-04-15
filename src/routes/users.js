0aac54daa2d3f9c3ca2620e9c4b6b77ccdd13a35
Leandro Helfer <leandrohelfer@gmail.com>
2023-04-14 21:33:33 -0300

    Nova rota para criar usuario no banco de dados

--- src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const conectarBancoDados = require('../middlewares/conectarBD');
-const router = express.Router();
-
-router.get('/', conectarBancoDados, function(req, res, next) {
-    res.send('respond with a resource 50' +process.env.TEST)
-})
-
-module.exports = router;
